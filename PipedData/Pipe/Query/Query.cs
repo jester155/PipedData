75f543f6cde49bcff80707f05b77f79df74e659a
Mark Provenzano <mark@dreambound.us>
2015-01-18 18:12:52 -0500

    Added query functionality, still have to handle the update condition

--- PipedData/Pipe/Query/Query.cs
+++ PipedData/Pipe/Query/Query.cs
@@ -1,10 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Pipe.Query {
-	class Query {
+
+	public struct Filter {
+		public string FilterColumn { get; set; }
+		public string FilterValue { get; set; }
+		public FileterOptions FilterOption { get; set; }
+	}
+
+	public class Query {
+
+		public Query() { }
+		public Query(Filter filter) {
+			this.Filter = filter;
+		}
+
+
+		public QueryOptions QueryOption { get; set; }
+		public Filter Filter { get; set; }
+		public string[] QueryParameters { get; set; }
+		public bool HasFilter { get; set; }
 	}
 }
