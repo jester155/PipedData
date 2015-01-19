1391b4edc1547ec4111e0263ec8e7fb751174727
Mark Provenzano <mark@dreambound.us>
2015-01-18 19:30:54 -0500

    Added database functionality and DatabaseFactory

---
+++ PipedData/Pipe/DatabaseFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Pipe {
+	class DatabaseFactory {
+
+		public string Datafile { get; set; }
+		private StreamReader DatabaseReader { get; set; }
+		const char DELIN = '|';
+
+		public DatabaseFactory(string dataFile) {
+			this.Datafile = dataFile;
+			this.DatabaseReader = new StreamReader(this.Datafile);
+		}
+
+		public Database MakeDatabase() {
+			var database = new Database() {
+				DataFile = this.Datafile ,
+				Headers = GetDatabaseHeaders() ,
+				Entries = GetDatbaseEntries()
+			};
+
+			this.DatabaseReader.Close();
+
+			return database;
+		}
+
+		public string[] GetDatabaseHeaders() {
+			return SplitOnPipe(this.DatabaseReader.ReadLine());
+		}
+
+		public string[][] GetDatbaseEntries() {
+			return this.DatabaseReader.ReadToEnd()
+				.Split(new string[] { Environment.NewLine } , StringSplitOptions.None)
+				.Select(line => SplitOnPipe(line))
+				.ToArray();
+		}
+
+		private string[] SplitOnPipe(string line) {
+			return line.Split(DELIN);
+		}
+	}
+}
